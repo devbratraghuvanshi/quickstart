528d930b0257563283f6c9b3ed8455d12e6ba4a8
devbratraghuvanshi <devbratcs0017@gmail.com>
2017-06-17 22:58:45 +0530

    add update and delete with http

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -7,7 +7,7 @@ import { Hero } from './hero';
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: [ './heroes.component.css' ],
+  styleUrls: ['./heroes.component.css'],
   providers: [HeroService]
 
 })
@@ -19,9 +19,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   constructor(
-              private heroService: HeroService,
-              private router: Router
-              ) {
+    private heroService: HeroService,
+    private router: Router
+  ) {
     // this.heroes = this.heroService.getHeroes(); // or this
     // this.getHeroes();   
     // they both will work but
@@ -32,10 +32,27 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   };
   getHeroes(): void {
-   this.heroService.getHeroesSlowly().then( heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   };
   gotoDetail(): void {
-  this.router.navigate(['/detail', this.selectedHero.id]);
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+  this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
 }
 }
 
